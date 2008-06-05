4950bc023305c44b1ba5df8b69252c184bb7a9f2
cHoBi <chobi.noob@gmail.com>
2008-06-05 17:36:53 +0200

    Changed license to AGPLv3.

--- install/index.php
+++ install/index.php
@@ -1,9 +1,23 @@
 <?php
 /**
-* Package  Misc
-* Author   cHoBi
-* License  http://opensource.org/licenses/gpl-3.0.html
-**/
+* @package  Install
+
+* @license AGPLv3
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+* @author   cHoBi
+*/
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
